d5d40bd4713f3ff964e51c99f76c382fbc58a606
AndyGeek <andy.eulogio@gmail.com>
2020-10-31 23:09:57 -0500

    Add axios

---
+++ src/views/Grid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <button @click="getPokemon">Click me</button>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+import axios from "axios"
+export default {
+  methods: {
+    async getPokemon(){
+      console.log(axios);
+      const datos = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+      console.log(datos);
+    }
+  }
+}
+</script>
